f798a33a2c89401a9f9f6fe3adf74248b64f534c
iakovlysenko <lysenko_iakov@yahoo.com>
2025-02-08 00:01:38 +0300

    Метод start(), exit() и метод guessLetter()

---
+++ src/main/java/ru/project/Game.java
@@ -0,0 +1,36 @@
+package ru.project;
+
+import java.util.Scanner;
+
+public class Game {
+    int numberOfLetters;
+    void start() {
+        System.out.println("Игра \"Виселица\" запущена. \n" +
+                "Для начала новой игры используйте комманду /start\n" +
+                "Для выхода используйте комманду /exit");
+        try(Scanner scanner = new Scanner(System.in)) {
+            String command = scanner.nextLine();
+            if (command.equals("start")) {
+                System.out.println("Загадано слово, которое содержит "
+                        + numberOfLetters + " букв.\n" +
+                        "Предположите букву.");
+            }
+            if (command.equals("exit")) {
+                exit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    void exit() {
+
+    }
+    void guessLetter() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            String guessWord = scanner.nextLine();
+            while (guessWord.length() != 1) {
+                System.out.println("Введите только одну букву!");
+            }
+        }
+    }
+}
