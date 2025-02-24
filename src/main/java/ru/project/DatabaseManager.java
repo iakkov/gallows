6d823d3ebf05d481364eefbc4925fd49c1eb1001
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-02-24 22:29:45 +0300

    Уровни сложности

--- src/main/java/ru/project/DatabaseManager.java
+++ src/main/java/ru/project/DatabaseManager.java
@@ -12,44 +12,46 @@ public class DatabaseManager {
             String createTableSQL = """
                    CREATE TABLE IF NOT EXISTS wordsForGallows (
                         id SERIAL PRIMARY KEY,
-                        word VARCHAR(255) NOT NULL
+                        word VARCHAR(255) NOT NULL,
+                        lvl INTEGER NOT NULL
                             );
                    """;
             String addWords = """
-                    INSERT INTO wordsForGallows (word) VALUES
-                    ('абрикос'), ('бабочка'), ('верблюд'), ('гвоздика'),
-                    ('деревня'), ('ежевика'), ('жасмин'), ('зебра'),
-                    ('игрушка'), ('йогурт'), ('капуста'), ('лампа'),
-                    ('малина'), ('небо'), ('огурец'), ('помидор'),
-                    ('речка'), ('самолет'), ('тумбочка'), ('улитка'),
-                    ('фонарь'), ('хоккей'), ('цветок'), ('черепаха'),
-                    ('шалаш'), ('щука'), ('эскимо'), ('юла'),
-                    ('яблоко'), ('барсук'), ('виноград'), ('горка'),
-                    ('дождь'), ('ежедневник'), ('жираф'), ('забор'),
-                    ('искра'), ('календарь'), ('лимон'), ('морковь'),
-                    ('носок'), ('облако'), ('палатка'), ('ракета'),
-                    ('собака'), ('трактор'), ('узор'), ('флаг'),
-                    ('хлеб'), ('чайник');
-                    """;
+                    INSERT INTO wordsForGallows (word, lvl) VALUES
+            ('абрикос', 1), ('бабочка', 1), ('верблюд', 2), ('гвоздика', 2),
+            ('деревня', 1), ('ежевика', 1), ('жасмин', 2), ('зебра', 1),
+            ('игрушка', 1), ('йогурт', 2), ('капуста', 1), ('лампа', 1),
+            ('малина', 1), ('небо', 1), ('огурец', 1), ('помидор', 1),
+            ('речка', 1), ('самолет', 2), ('тумбочка', 3), ('улитка', 1),
+            ('фонарь', 2), ('хоккей', 2), ('цветок', 1), ('черепаха', 3),
+            ('шалаш', 1), ('щука', 1), ('эскимо', 2), ('юла', 1),
+            ('яблоко', 1), ('барсук', 2), ('виноград', 2), ('горка', 1),
+            ('дождь', 1), ('ежедневник', 3), ('жираф', 1), ('забор', 1),
+            ('искра', 1), ('календарь', 2), ('лимон', 1), ('морковь', 1),
+            ('носок', 1), ('облако', 1), ('палатка', 2), ('ракета', 2),
+            ('собака', 1), ('трактор', 2), ('узор', 1), ('флаг', 1),
+            ('хлеб', 1), ('чайник', 2);
+            """;
             stmt.execute(createTableSQL);
             stmt.executeUpdate(addWords);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    public String secretWord() {
-        String query = "SELECT word FROM wordsForGallows ORDER BY RANDOM() LIMIT 1;";
+    public String secretWord(int level) {
+        String query = "SELECT word FROM wordsForGallows WHERE lvl = ? ORDER BY RANDOM() LIMIT 1;";
         String word = null;
 
         try (Connection connection = DriverManager.getConnection(DB_URL, "postgres", "postgres");
-             PreparedStatement statement = connection.prepareStatement(query);
-             ResultSet resultSet = statement.executeQuery()) {
+             PreparedStatement statement = connection.prepareStatement(query)) {
 
-            if (resultSet.next()) {
-                word = resultSet.getString("word");
-                numberOfLetters = word.length();
-            } else {
-                throw new RuntimeException("Таблица wordsForGallows пуста.");
+            statement.setInt(1, level);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    word = resultSet.getString("word");
+                } else {
+                    throw new RuntimeException("Нет слов для выбранного уровня сложности");
+                }
             }
 
         } catch (SQLException e) {
